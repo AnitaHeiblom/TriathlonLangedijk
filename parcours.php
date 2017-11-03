2512decc9ac4e25fbdec032463813c6484f00493
anitaheiblom <anitaheiblom@gmail.com>
2017-11-03 18:16:02 +0100

    Added news

--- parcours.php
+++ parcours.php
@@ -24,21 +24,20 @@
                     <iframe src="https://www.google.com/maps/d/embed?mid=180ahx2PopqDdr8ORhAi_rw4tgajFgTE4" width="640"
                             height="480"></iframe>
                 </div>
-                <p>
+
                 <ol>
                     <li>Start is bij de sluis</li>
                     <li>250m rechtdoor zwemmen tot aan het keerpunt</li>
                     <li>Na keerpunt terugzwemmen naar steiger bij De Buizerd</li>
                     <li>Uit het water bij de steiger en wisselen in de wisselzone</li>
                 </ol>
-                De deelnemers moeten rechts van de zwemlijnen zwemmen.</p>
+                <p>De deelnemers moeten rechts van de zwemlijnen zwemmen.</p>
 
                 <h3>Fietsparcours</h3>
                 <div class="maps-container">
                     <iframe src="https://www.google.com/maps/d/embed?mid=1GHnVkSshT97ASLk6qsixq8OOWdiVvxAq" width="640"
                             height="480"></iframe>
                 </div>
-                <p>
                 <ol>
                     <li>Vanuit de wisselzone rechtsaf richting de oude Provinciale weg</li>
                     <li>Oude Provinciale weg uitrijden tot aan de Smuigelweg</li>
@@ -46,7 +45,8 @@
                     <li>Linksaf naar de Waarddijk</li>
                     <li>Waarddijk uitrijden tot aan Provinciale weg</li>
                 </ol>
-                Er dienen 4 rondes gereden te worden. Na het 4<sup>e</sup> rondje rechtsaf slaan en naar de wisselzone
+                <p>Er dienen 4 rondes gereden te worden. Na het 4<sup>e</sup> rondje rechtsaf slaan en naar de
+                    wisselzone
                 toerijden. De deelnemers moeten zoveel mogelijk links rijden.</p>
 
                 <h3>Loopparcours</h3>
@@ -54,15 +54,14 @@
                     <iframe src="https://www.google.com/maps/d/embed?mid=1_o8GdmoKFAsIt8mEWHgWKBTHpaLsL9Tg" width="640"
                             height="480"></iframe>
                 </div>
-                <p>
                 <ol>
                     <li>Vanuit de wisselzone rechts richting het fietspad</li>
-                    <li>Het fietspas aflopen tot an de Dorpsstraat</li>
+                    <li>Het fietspad aflopen tot an de Dorpsstraat</li>
                     <li>Rechtsaf de Dorpsstraat uitlopen tot aan keerpunt afslag Woudmeerweg (caf&eacute; de Knip)</li>
                     <li>Omkeren en teruglopen over de Dorpsstraat tot aan de Spoorstraat</li>
                     <li>Rechtsaf de Spoorstraat inlopen en dan uitlopen tot aan de finish</li>
                 </ol>
-                De deelnemers moeten zoveel mogelijk <strong>links</strong> van de weg blijven lopen.</p>
+                <p>De deelnemers moeten zoveel mogelijk <strong>links</strong> van de weg blijven lopen.</p>
 
 
             </div>
--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,38 @@
 <div class="col-12 col-lg-4 col-md-3 sm-12">
     <div class="side-bar">
         <h2>Nieuws</h2>
-        <p>Incididunt sirloin chicken biltong do aliqua doner bacon kevin shoulder dolore. Cillum ullamco
-            t-bone, ball tip pancetta dolor tempor corned beef aute pork chop. Labore commodo strip steak
-            chuck, dolore alcatra burgdoggen ut. Pancetta exercitation sed shankle bacon ipsum laborum pork
-            chop biltong qui t-bone occaecat nostrud. In incididunt ham hock, dolore lorem occaecat
-            cupidatat venison boudin deserunt short ribs in hamburger velit magna. Shank turducken
-            landjaeger alcatra non qui kielbasa in hamburger picanha laboris meatball doner labore.</p>
+        <p>Voor de zevende keer staat de Triathlon Langedijk in 2018 op de kalender! Deze editie wordt
+            op woensdagavond 20 juni 2018 georganiseerd door Stichting
+            Triathlon Langedijk in samenwerking met Triathlonvereniging
+            Zwefilo. Vanaf vrijdag 3 februari 2018 om 00.00 uur kunnen sportievelingen zich
+            inschrijven om mee te doen.
+            Het is een laagdrempelig, recreatief evenement over de kortste
+            triathlon afstanden circa 600m zwemmen, 18,4km fietsen en 5km lopen. Deelname is individueel mogelijk, maar
+            ook als team
+            van twee of drie atleten, waarbij een ieder één of twee van de
+            onderdelen voor zijn&#47;haar rekening neemt. Dus iedereen die een
+            beetje sportief is, kan meedoen! De start en finish van het
+            evenement zijn bij Hotel Restaurant “De Buizerd” aan de
+            Spoorstraat in Noord&ndash;Scharwoude. Hier is ook de wisselzone.</p>
+
+        <h4>Publiek</h4>
+        <p>Het zwemmen zal zijn direct voor De Buizerd in het kanaal
+            Alkmaar&ndash;Kolhorn, daarna wordt er gefietst op een parcours aan de
+            overzijde van het kanaal waarbij de deelnemers een viertal ronden
+            moeten afleggen. Als laatste wordt er gelopen door de Spoorstraat
+            en de Dorpsstraat in Oudkarspel met finish direct voor De
+            Buizerd. Ook voor het publiek is het evenement prima te volgen,
+            doordat de atleten meerdere malen voorbij komen. Dat bleek de
+            afgelopen jaren toen het publiek in grote getale langs het
+            parcours stond. De eerste start is om 19.30 uur.</p>
+
+        <h4>Veel belangstelling</h4>
+        <p>De animo voor deelname was de vorige edities groot. De
+            individuele inschrijving is snel vol, dus wees er snel bij. Ook voor
+            de deelname als team is veel belangstelling, ook een reden om
+            direct in te schrijven. Je kunt deelnemen als gezin, vrienden of
+            bedrijfsteam, waarbij niet alleen het winnen, maar vooral ook het
+            meedoen en de gezelligheid</p>
+
     </div>
 </div>
